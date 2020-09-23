84cb188ac144cbaead09b2974b02f27bb5d456d6
Richard Rosario <tech2js@gmail.com>
2020-09-23 19:23:29 +0800

    creating models

--- personal/views.py
+++ personal/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
+from personal.models import Question
 
 # home view
 
 
 def home_screen_view(request):
     context = {}
-    context['some_string'] = 'This is some string passed from the views'
-
-    list_of_values = []
-    list_of_values.append('entry1')
-    list_of_values.append('entry2')
-    list_of_values.append('entry3')
-    list_of_values.append('entry4')
-    context['list_of_values'] = list_of_values
+    questions = Question.objects.all()
+    context['questions'] = questions
 
     return render(request, 'personal/home.html', context)
