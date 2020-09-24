d126f5a2d0e99c3d117d4fcf0343357917c000f4
Richard Rosario <tech2js@gmail.com>
2020-09-24 22:44:13 +0800

    added profile/account view

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,12 +6,15 @@ from personal.views import (
 from account.views import (
     registration_view,
     logout_view,
-    login_view
+    login_view,
+    account_view
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_screen_view, name='home'),
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('account/', account_view, name='account'),
+
 ]
