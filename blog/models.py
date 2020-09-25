d12c3ef91f0b4b8abe03a4d8fc862ef1577ff44a
Richard Rosario <tech2js@gmail.com>
2020-09-26 00:39:37 +0800

    setup static files

--- blog/models.py
+++ blog/models.py
@@ -8,21 +8,18 @@ from django.dispatch import receiver
 
 def upload_location(instance, filename, **kwargs):
     file_path = 'blog/{author_id}/{title}-{filename}'.format(
-        author_id=str(instance.author.id, title=str(
-            instance.title), filename=filename)
-    )
+        author_id=str(instance.author.id), title=instance.title, filename=filename)
     return file_path
 
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
-    body = models.CharField(max_length=5000, null=False, blank=False)
-    image = models.ImageField(
-        upload_to=upload_location, null=False, blank=False)
+    body = models.TextField(max_length=5000, null=False, blank=False)
+    image = models.ImageField(upload_to=upload_location, null=True, blank=True)
     date_published = models.DateTimeField(
-        auto_now_add=True, verbose_name='date published')
+        auto_now_add=True, verbose_name="date published")
     date_updated = models.DateTimeField(
-        auto_now=True, verbose_name='date updated')
+        auto_now=True, verbose_name="date updated")
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug = models.SlugField(blank=True, unique=True)
