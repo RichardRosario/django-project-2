c5242198f3697cbb2662196d9ea2ea45a313cf42
Richard Rosario <tech2js@gmail.com>
2020-09-29 20:15:46 +0800

    added edit post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from blog.views import (
     create_blog_view,
-    detail_blog_view
+    detail_blog_view,
+    edit_blog_view
 )
 
 app_name = 'blog'
@@ -9,5 +10,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('create/', create_blog_view, name='create'),
-    path('<slug>/', detail_blog_view, name='detail')
+    path('<slug>/', detail_blog_view, name='detail'),
+    path('<slug>edit/', edit_blog_view, name='edit')
+
 ]
